a24d94069ef6c3254f79430cf9f4b2f0d1793b7e
Coder Tester <codewithme@outlook.com>
2016-04-17 09:05:41 +0530

    first commit for webtables

---
+++ src/main/java/com/codewitme/webtables/WebTable.java
@@ -0,0 +1,76 @@
+package com.codewitme.webtables;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class WebTable {
+	
+	WebDriver driver;
+	
+	public WebTable(WebDriver driver){
+		this.driver=driver;
+	}
+	
+	
+	public WebElement tableElement(){
+		return  driver.findElement(By.id("table1"));
+	}
+	
+	public List<String> getColumnNames(){
+		List<String> colNames=new ArrayList();
+		List<WebElement> headerElements=tableElement().findElement(By.tagName("thead")).findElement(By.tagName("tr")).findElements(By.tagName("th"));
+		for(WebElement headerElement:headerElements){
+			String columnName=headerElement.findElement(By.tagName("span")).getText();
+			colNames.add(columnName);
+		}
+		return colNames;
+	}
+	
+	public List<WebElement> getColumnElements(){
+		return tableElement().findElement(By.tagName("thead")).findElement(By.tagName("tr")).findElements(By.tagName("th"));
+	}
+	
+	public List<WebElement> getRowElements(){
+		return tableElement().findElement(By.tagName("tbody")).findElements(By.tagName("tr"));
+	}
+	
+	public Integer getColIndexByColName(String columnName){
+		int index;
+		List<WebElement> columnElements=getColumnElements();
+		for(int i=0;i<columnElements.size();i++){
+			String colName=columnElements.get(i).findElement(By.tagName("span")).getText().trim();
+			if(colName.equals(columnName)){
+				index=i;
+				return index;
+			}
+		}
+		
+		return null;
+	}
+	
+	public String getCellValue(String columnName,Integer rowIndex){
+		int colIndex = getColIndexByColName(columnName);//colIndex=2,Email
+		return getRowElements().get(rowIndex).findElements(By.tagName("td")).get(colIndex).getText();
+	}
+	
+	public Integer getRowIndex(String ColumnName,String expectedCellValue){
+		int colIndex = getColIndexByColName(ColumnName);//colIndex=1
+		int rowIndex = -1;
+		List<WebElement> rowElements=getRowElements();
+		//Iterating rows tr
+		for(int rows=0;rows<getRowElements().size();rows++){
+			//Iterating through each td
+			List<WebElement> datas=rowElements.get(rows).findElements(By.tagName("td"));
+			String actualCellData=datas.get(colIndex).getText();
+			if(expectedCellValue.equals(actualCellData)){
+				rowIndex=rows;
+			}
+		}
+		return rowIndex;		
+	}
+
+}
---
+++ src/test/java/com/codewitme/tests/WebTableTest.java
@@ -0,0 +1,53 @@
+package com.codewitme.tests;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.codewitme.webtables.WebTable;
+
+public class WebTableTest {
+	
+	WebDriver driver;
+	WebTable table;
+	@BeforeMethod
+	public void beforeClass(){
+		driver = new FirefoxDriver();
+		driver.get("http://the-internet.herokuapp.com/tables");
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		table=new WebTable(driver);
+	}
+	@AfterMethod
+	public void afterClass(){
+		driver.close();
+		driver.quit();
+	}
+	@Test(enabled=false)
+	public void verifyColumnNames(){
+		String[] expectedColNames= new String []{"Last Name","First Name"};
+		//table=new WebTable(driver); 
+		List<String> actualColNames=table.getColumnNames();
+		Assert.assertArrayEquals(expectedColNames, actualColNames.toArray());
+		
+	}
+	
+	@Test
+	public void verifyCellValue(){
+		
+		//table=new WebTable(driver); 
+		//int index=table.getColIndexByColName("Last Name");
+		int rowIndex=table.getRowIndex("Last Name", "Bach");
+		String actualEmail=table.getCellValue("Email", rowIndex);
+		Assert.assertEquals("Faild: The email id is incorrect", "fbach@yahoo.com", actualEmail);
+	}
+
+}
